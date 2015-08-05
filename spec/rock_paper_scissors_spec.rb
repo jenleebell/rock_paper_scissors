b796688133888d32cd4367724c8ddf8532c1c7f0
Jennifer Bell & Ryan Heupler <student@epicodus.com>
2015-08-05 14:46:44 -0700

    added third spec for player 2 winning

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -9,7 +9,13 @@ describe('String#rock_paper_scissors') do
 end
 
 describe('String#rock_paper_scissors') do
-    it("returns true if rock is the object and scissors is the argument") do
+    it("returns tie if both answers are identical") do
       expect("paper".beats?("paper")).to(eq("Yo its a tie"))
     end
+end
+
+describe('String#rock_paper_scissors') do
+  it("returns player 2 as winner if scissors beats rock") do
+    expect("scissors".beats?("rock")).to(eq("player 2 wins"))
+  end
 end
